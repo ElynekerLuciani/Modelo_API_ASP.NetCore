bb8c5bad9d2c0197002c93f56b0b2ca9752854f5
Elyneker Luciani <elyneker@gmail.com>
2021-09-22 17:39:38 -0300

    implementando teste de delete

---
+++ src/Api.Application.Test/Usuario/QuandoRequisitarGet/Retorno_BadRequest.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Threading.Tasks;
+using Api.Application.Controllers;
+using Api.Domain.DTOs.User;
+using Api.Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace Api.Application.Test.Usuario.QuandoRequisitarGet
+{
+    public class Retorno_BadRequest
+    {
+        private UsersController _controller;
+
+        [Fact(DisplayName = "É possível realizar o Get")]
+        public async Task E_Possivel_Invocar_A_Controller_Get()
+        {
+
+            var serviceMock = new Mock<IUserServices>();
+            var nome = Faker.Name.FullName();
+            var email = Faker.Internet.Email();
+
+            serviceMock.Setup(m => m.Get(It.IsAny<Guid>())).ReturnsAsync(
+                new UserDTO
+                {
+                    Id = Guid.NewGuid(),
+                    Name = nome,
+                    Email = email,
+                    CreateAt = DateTime.UtcNow
+                }
+            );
+
+            _controller = new UsersController(serviceMock.Object);
+            _controller.ModelState.AddModelError("Id", "Formato Inválido");
+
+            var result = await _controller.Delete(default(Guid));
+            Assert.True(result is BadRequestObjectResult);
+
+        }
+    }
+}
